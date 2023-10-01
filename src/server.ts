ccdad3a1934e36313c27a3af695dae1622b955fc
AlysonDEV <devalyson@gmail.com>
2023-10-01 18:16:00 -0300

    bug no API OPENAI

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
-import { fastify } from 'fastify'
+import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionsRoute } from "./routes/create-transciption";
 
+const app = fastify();
 
-const app = fastify()
+app.register(getAllPromptsRoute);
+app.register(createTranscriptionsRoute);
+app.register(uploadVideoRoute);
 
-app.get('/', () => {
-  return 'hello world'
-})
-
-app.listen({
-  port: 3333,
-}).then(()=>{
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server Running!");
+  });
